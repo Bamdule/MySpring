d692f462d32c268732a9cbefea42a5fb256e28a0
Bamdule <Bamdule5@gmail.com>
2019-02-10 18:06:04 +0900

    2019-02-10 설정

--- src/main/java/com/bamdule/controller/TestController.java
+++ src/main/java/com/bamdule/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.bamdule.controller;
 
+import com.bamdule.dao.GuestBookDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,8 +13,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class TestController {
 
+    @Autowired
+    private GuestBookDao guestBookDao;
+
     @RequestMapping(value = "/home", method = RequestMethod.GET)
     public String test() {
+
+        System.out.println(guestBookDao);
         return "/WEB-INF/views/hello.jsp";
     }
 
---
+++ src/main/java/com/bamdule/dao/impl/GuestBookDaoImpl.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bamdule.dao.impl;
+
+import com.bamdule.dao.GuestBookDao;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author MW
+ */
+@Repository
+public class GuestBookDaoImpl implements GuestBookDao {
+
+}
