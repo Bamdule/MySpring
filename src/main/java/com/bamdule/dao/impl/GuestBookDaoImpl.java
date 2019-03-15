b1d12fb4e35979e3adb4ab07f132e361b0b41ddd
MW <Bamdule5@gmail.com>
2019-03-15 18:36:38 +0900

    개발 환경별 properties 적용

--- src/main/java/com/bamdule/dao/impl/GuestBookDaoImpl.java
+++ src/main/java/com/bamdule/dao/impl/GuestBookDaoImpl.java
@@ -1,22 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.bamdule.dao.impl;
 
 import com.bamdule.dao.GuestBookDao;
-import com.bamdule.model.vo.GuestBookVO;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import javax.sql.DataSource;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 /**
